6c059411da15126af3abadfb25af00f04b2a274e
MDutro <mdutrolaw@gmail.com>
2020-04-23 21:19:32 -0500

    Initial commit. Add external API Add state management for API data.

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const KEY = process.env.KEY;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const url = ` http://api.weatherbit.io/v2.0/forecast/daily?key=${KEY}&days=8&units=I&lat=${req.query.q[0]}&lon=${req.query.q[1]}`
+  axios.get(url)
+  .then(response => response.data)
+  .then(data => res.send(data))
+  .catch(err => console.log(err))
+});
+
+
+
+app.listen(3001, console.log("server listening on port 3001"));
--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [gps, setGps] = useState('')
+  const [weather, setWeather] = useState({})
+  
+  useEffect(() => navigator.geolocation.getCurrentPosition(
+    position => setGps([position.coords.latitude.toFixed(2), position.coords.longitude.toFixed(2)])
+    // position => setGps(`${position.coords.latitude.toFixed(2)},${position.coords.longitude.toFixed(2)}`)
+   ) , [])
+
+   useEffect(() => {
+     if (gps) {
+      axios.get('http://localhost:3001', {
+        params: {
+          q: gps
+        }
+      })
+      .then(response => response.data)
+      .then(data => setWeather(data))
+      .catch(err => console.log(err))
+     } 
+  }, [gps])
+ console.log(weather)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>GoodDayBiker</h1>
     </div>
   );
 }
