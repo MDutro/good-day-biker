765e58d46f4509bede2c2497949ce75f92d4bd80
Micah Dutro <mdutrolaw@gmail.com>
2020-04-28 13:03:35 -0500

    Adjust UserPreferences.css and UserPreferences.js animations.

--- src/UserPreferences.js
+++ src/UserPreferences.js
@@ -11,25 +11,30 @@ const UserPreferences = (props) => {
   // const initialState = () => ({high: 65, rain: 25, wind: 10})  
   // const [preferences, setPreferences] = useState(intitialState())
 
+  const hidePrefs = () => {
+    document.querySelector(".preferences").classList.add('close');
+    setTimeout(props.toggleSettings(), 0)
+  }
+
   return(
     <div className="preferences">
       <h1>User Preferences</h1>
       <p>Minimum temperature</p>
-      <div class="sliderContainer">
-        <input type="range" min="-40" max="110" value={preferences.high} className="slider" 
+      <div className="sliderContainer">
+        <input type="range" min="-40" max="110" defaultValue={preferences.high} className="slider" 
           onChange={e => preferences.high = e.target.value} />
       </div>
       <p>Maximum precipitaion chance</p>
-      <div class="sliderContainer">
-        <input type="range" min="0" max="100" value={preferences.rain} className="slider"
+      <div className="sliderContainer">
+        <input type="range" min="0" max="100" defaultValue={preferences.rain} className="slider"
           onChange={e => preferences.rain = e.target.value} />
       </div>
       <p>Maximum wind speed</p>
-      <div class="sliderContainer">
-        <input type="range" min="0" max="50" value={preferences.wind} className="slider"
+      <div className="sliderContainer">
+        <input type="range" min="0" max="50" defaultValue={preferences.wind} className="slider"
           onChange={e => preferences.wind = e.target.value} />
       </div>
-      <button onClick={props.toggleSettings} className="button">Save</button>
+      <button onClick={hidePrefs} className="button">Save</button>
     </div>
   )
 }
